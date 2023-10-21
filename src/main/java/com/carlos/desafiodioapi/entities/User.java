e24c5d630b2e550b5906e54fa4232bb963d1137b
carlos4565mana <carloscal61@gmail.com>
2023-10-21 21:13:15 -0200

    Domain model

---
+++ src/main/java/com/carlos/desafiodioapi/entities/User.java
@@ -0,0 +1,43 @@
+package com.carlos.desafiodioapi.entities;
+
+public class User {
+	
+	private Long id;
+	private String email;
+	
+	
+	public User() {
+		super();
+	}
+
+
+	public User(Long id, String email) {
+		super();
+		this.id = id;
+		this.email = email;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
